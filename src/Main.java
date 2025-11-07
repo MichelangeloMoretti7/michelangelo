13b5aa511f0ac0d222e6c5801b3084a514d40425
giuliamenca07 <giuliamenca07@gmail.com>
2025-11-07 13:02:12 +0100

    primo commit corsacavalli moretti

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
-    public static String primo = ""
+    public static String primo = "";
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
